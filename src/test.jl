a77df4ef79d69efca80c117d9a43876efe666e61
Mike Nolta <mike@nolta.net>
2012-12-03 21:27:58 -0500

    start work on tests

---
+++ src/test.jl
@@ -0,0 +1,9 @@
+require("Calendar")
+using Calendar
+
+# daylight savings time
+t = ymd_hms(2013, 3, 10, 1, 59, 59)
+t2 = t + seconds(1)
+@assert hour24(t2) == 3
+@assert minute(t2) == 0
+@assert second(t2) == 0
