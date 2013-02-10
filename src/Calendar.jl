3de2a044c3aef4c2f4d3b840c365b8a3d80d1e20
Tom Short <t.short@ieee.org>
2013-02-10 08:16:37 -0500

    Add isless for CalendarTime.

--- src/Calendar.jl
+++ src/Calendar.jl
@@ -61,7 +61,7 @@ export CalendarTime,
 
 import Base.show, Base.(+), Base.(-), Base.(<), Base.(==), Base.length,
        Base.colon, Base.ref, Base.start, Base.next, Base.done, Base.(*), Base.(.*),
-       Base.size, Base.step, Base.vcat
+       Base.size, Base.step, Base.vcat, Base.isless
 
 type CalendarTime
     millis::Float64
@@ -174,7 +174,7 @@ am(t::CalendarTime) = !pm(t)
 @vectorize_1arg CalendarTime am
 @vectorize_1arg CalendarTime pm
 
-for op in [:<, :(==)]
+for op in [:<, :(==), :isless]
     @eval ($op)(t1::CalendarTime, t2::CalendarTime) = ($op)(t1.millis, t2.millis)
 end
 
@@ -426,5 +426,16 @@ const September = 9
 const October = 10
 const November = 11
 const December = 12
+
+## _default_formats = ["yyyy-MM-dd HH:mm:ss.SSS z",
+##                     "yyyy-MM-dd HH:mm:ss.SSS",
+##                     "yyyy-MM-dd HH:mm:ss z",
+##                     "yyyy-MM-dd HH:mm:ss",
+##                     "yyyy-MM-dd HH:mm z",
+##                     "yyyy-MM-dd HH:mm",
+##                     "yyyy-MM-dd z",
+##                     "yyyy-MM-dd"]
+##                     "yyyy-MM z",
+##                     "yyyy-MM"]
 
 end # module
